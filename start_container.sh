90f1f39607f5acd1c683ce3a688a3531169edd83
Lekhana-Gayatri <148931339+Lekhana-Gayatri@users.noreply.github.com>
2024-06-08 13:45:45 +0530

    changed

--- start_container.sh
+++ start_container.sh
@@ -4,5 +4,5 @@ set -e
 docker pull lekhana2004/django-code-build:v1
 echo
 
-CONTAINERID=$(docker run -d -p 8000:8000 lekhana2004/django-code-build:v1)
-echo
+CONTAINERID=$(docker run -d -p 8000:8000 --label demoBuildContainer lekhana2004/django-code-build:v1)
+echo $CONTAINERID
--- stop_container.sh
+++ stop_container.sh
@@ -2,6 +2,7 @@
 set -e
 
 # Stop the running container (if any)
+CONTAINERID=$(docker ps -q --filter "label=demoBuildContainer")
 docker stop $CONTAINERID
 docker rm $CONTAINERID
 echo "Hi"

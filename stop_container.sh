af904a5d62a5a35b7f19386ffc83e47e9494344e
Lekhana-Gayatri <148931339+Lekhana-Gayatri@users.noreply.github.com>
2024-06-08 14:06:24 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -3,7 +3,14 @@ set -e
 
 # Stop the running container (if any)
 CONTAINERID=$(docker ps -q --filter "label=demoBuildContainer")
-echo $CONTAINERID
-docker stop $CONTAINERID
-docker rm $CONTAINERID
+echo "Found container ID: $CONTAINERID"
+
+if [ -n "$CONTAINERID" ]; then
+  docker stop $CONTAINERID
+  docker rm $CONTAINERID
+  echo "Container stopped and removed"
+else
+  echo "No container found with label demoBuildContainer"
+fi
+
 echo "Hi"

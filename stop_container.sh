fb305c0e59c4ee6135a8018556a94dd1e4efa570
Lekhana-Gayatri <148931339+Lekhana-Gayatri@users.noreply.github.com>
2024-06-08 13:57:15 +0530

    commit

--- stop_container.sh
+++ stop_container.sh
@@ -3,6 +3,7 @@ set -e
 
 # Stop the running container (if any)
 CONTAINERID=$(docker ps -q --filter "label=demoBuildContainer")
+echo $CONTAINERID
 docker stop $CONTAINERID
 docker rm $CONTAINERID
 echo "Hi"
